0f7f55824e5de871d0c3872b5fb88b28ffacc797
Krasko08 <204331648+Krasko08@users.noreply.github.com>
2025-11-20 15:26:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,106 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace LabWork
 {
-    // Даний проект є шаблоном для виконання лабораторних робіт
-    // з курсу "Об'єктно-орієнтоване програмування та патерни проектування"
-    // Необхідно змінювати і дописувати код лише в цьому проекті
-    // Відео-інструкції щодо роботи з github можна переглянути 
-    // за посиланням https://www.youtube.com/@ViktorZhukovskyy/videos 
-    class Program
+    public class GraphForm : Form
     {
-        static void Main(string[] args)
+        private readonly List<PointF> _points;
+
+        private const double XStart = 4.5;
+        private const double XEnd = 16.4;
+        private const double Step = 2.2;
+
+        public GraphForm()
         {
-            
-            Console.WriteLine("Hello World!");
+            Text = "Графік y = (x^3 - 2) / (3 ln(x))";
+            BackColor = Color.White;
+            DoubleBuffered = true;
+
+            _points = CalculatePoints();
+
+            Resize += (s, e) => Invalidate();
+        }
+
+        private List<PointF> CalculatePoints()
+        {
+            var points = new List<PointF>();
+
+            for (double x = XStart; x <= XEnd + 0.001; x += Step)
+            {
+                double y = (Math.Pow(x, 3) - 2) / (3 * Math.Log(x));
+                points.Add(new PointF((float)x, (float)y));
+            }
+
+            return points;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (_points.Count < 2)
+                return;
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            DrawAxes(g);
+            DrawGraph(g);
+        }
+
+        private void DrawAxes(Graphics g)
+        {
+            using Pen pen = new Pen(Color.Black, 2);
+
+            // OX
+            g.DrawLine(pen, 40, Height - 40, Width - 20, Height - 40);
+
+            // OY
+            g.DrawLine(pen, 40, 20, 40, Height - 40);
+        }
+
+        private void DrawGraph(Graphics g)
+        {
+            float minX = (float)XStart;
+            float maxX = (float)XEnd;
+
+            float minY = float.MaxValue;
+            float maxY = float.MinValue;
+
+            foreach (var p in _points)
+            {
+                minY = Math.Min(minY, p.Y);
+                maxY = Math.Max(maxY, p.Y);
+            }
+
+            float graphWidth = Width - 80;
+            float graphHeight = Height - 80;
+
+            using Pen pen = new Pen(Color.Red, 2);
+
+            for (int i = 0; i < _points.Count - 1; i++)
+            {
+                float x1 = 40 + ((_points[i].X - minX) / (maxX - minX)) * graphWidth;
+                float y1 = Height - 40 - ((_points[i].Y - minY) / (maxY - minY)) * graphHeight;
+
+                float x2 = 40 + ((_points[i + 1].X - minX) / (maxX - minX)) * graphWidth;
+                float y2 = Height - 40 - ((_points[i + 1].Y - minY) / (maxY - minY)) * graphHeight;
+
+                g.DrawLine(pen, x1, y1, x2, y2);
+            }
+        }
+    }
+
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new GraphForm());
         }
     }
 }
