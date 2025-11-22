358bc1f0248504a0c0f5db333efcae3926f186f5
Krasko08 <204331648+Krasko08@users.noreply.github.com>
2025-11-22 11:52:50 +0200

    Refactor GraphForm to Form1 and update drawing logic

--- Program.cs
+++ Program.cs
@@ -1,80 +1,55 @@
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace DynamicGraph
+namespace GraphPlotter
 {
-    public class GraphForm : Form
+    public partial class Form1 : Form
     {
-        private const double XStart = 1.2;
-        private const double XEnd = 6.3;
-        private const double Step = 0.2;
-
-        public GraphForm()
+        public Form1()
         {
-            this.Text = "Dynamic Graph";
+            InitializeComponent();
             this.Resize += (s, e) => this.Invalidate();
-            this.DoubleBuffered = true;
-        }
-
-        private double Function(double x)
-        {
-            return (5 * Math.Tan(x + 7)) / Math.Pow(x + 3, 2);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            Graphics g = e.Graphics;
-            g.Clear(Color.White);
+            DrawGraph(e.Graphics, this.ClientSize);
+        }
 
-            int w = this.ClientSize.Width;
-            int h = this.ClientSize.Height;
-            if (w < 50 || h < 50) return;
+        private void DrawGraph(Graphics g, Size clientSize)
+        {
+            g.Clear(Color.White);
 
             Pen axisPen = new Pen(Color.Black, 2);
             Pen graphPen = new Pen(Color.Blue, 2);
 
-            int x0 = w / 2;
-            int y0 = h / 2;
-            g.DrawLine(axisPen, 0, y0, w, y0);
-            g.DrawLine(axisPen, x0, 0, x0, h);
+            int centerX = clientSize.Width / 2;
+            int centerY = clientSize.Height / 2;
 
-            double minY = double.MaxValue;
-            double maxY = double.MinValue;
-            for (double x = XStart; x <= XEnd; x += Step)
+            g.DrawLine(axisPen, 0, centerY, clientSize.Width, centerY); // X-axis
+            g.DrawLine(axisPen, centerX, 0, centerX, clientSize.Height); // Y-axis
+
+            List<PointF> points = new List<PointF>();
+
+            for (double x = 1.2; x <= 6.3; x += 0.2)
             {
-                double y = Function(x);
-                if (y < minY) minY = y;
-                if (y > maxY) maxY = y;
+                double y = 5 * Math.Tan(x + 7) / Math.Pow(x + 3, 2);
+
+                float px = centerX + (float)(x * 50); // масштаб X
+                float py = centerY - (float)(y * 50); // масштаб Y
+
+                if (float.IsInfinity(py) || float.IsNaN(py)) continue;
+
+                points.Add(new PointF(px, py));
             }
 
-            double scaleX = w / (XEnd - XStart);
-            double scaleY = (h * 0.8) / (maxY - minY);
-
-            PointF? prev = null;
-            for (double x = XStart; x <= XEnd; x += Step)
+            if (points.Count > 1)
             {
-                double y = Function(x);
-
-                float screenX = (float)((x - XStart) * scaleX);
-                float screenY = (float)(h - (y - minY) * scaleY);
-
-                PointF current = new PointF(screenX, screenY);
-
-                if (prev != null)
-                {
-                    g.DrawLine(graphPen, prev.Value, current);
-                }
-                prev = current;
+                g.DrawLines(graphPen, points.ToArray());
             }
-        }
-
-        [STAThread]
-        public static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.Run(new GraphForm());
         }
     }
 }
